14113ded7611ac1c4b86c727b310999b6d0e1e22
Cryptouru <cryptouru@gmail.com>
2019-03-12 01:06:07 -0300

    Package create and eventBus tests

---
+++ components/Counter.vue
@@ -0,0 +1,54 @@
+
+<script>
+export default {
+  name: "Counter", // vue component name
+  data() {
+    return {
+      counter: 5,
+      initCounter: 5
+    };
+  },
+  props: {
+    name: {
+      type: String,
+      default: "default Name"
+    }
+  },
+  methods: {
+    handleEmmit: function(counter) {
+      window.eventBus.$emit("customStuff", {
+        message: "I emmit from " + this.name,
+        counter
+      });
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="counter">
+    <p>
+      The counter is set to
+      <b>{{counter}}</b>.
+    </p>
+    <button @click="counter += 1">Click +1</button>
+    <button @click="counter -= 1">Click -1</button>
+    <button @click="counter = initCounter">Reset</button>
+    <button @click="handleEmmit(counter)">Emmit</button>
+  </div>
+</template>
+
+<style scoped>
+.counter {
+  display: block;
+  width: 400px;
+  margin: 25px auto;
+  border: 1px solid #ccc;
+  background: #eaeaea;
+  text-align: center;
+  padding: 25px;
+}
+.counter p {
+  margin: 0 0 1em;
+}
+</style>
---
+++ components/Counters.vue
@@ -0,0 +1,38 @@
+
+<script>
+import Counter from "./Counter";
+export default {
+  name: "Counters", // vue component name
+  components: {
+    Counter
+  },
+  data() {
+    return {
+      counters: []
+    };
+  },
+  props: {
+    data: Array
+  },
+  mounted() {
+    console.log("test");
+    console.log(this);
+    console.log(typeof this.data);
+    console.log(this.data);
+    if (globalCounters) {
+      this.counters = globalCounters;
+      console.log(this.counters);
+      console.log(globalCounters);
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="counter">
+    <counter :name="'happy counter'"/>
+    <counter :name="'sad counter'"/>
+    <counter/>
+    <!-- <counter v-for="counter of test" v-bind:key="counter" :name="counter"/> -->
+  </div>
+</template>
